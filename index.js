dd7e5eceb9711e069a17f14abf95e6752bd4ba4e
Benjamin Arruebarrena <arruebarrena15@gmail.com>
2022-03-23 11:21:47 -0300

    creand las constantes

---
+++ index.js
@@ -0,0 +1,7 @@
+//creando las constantes //
+
+const displayValorAnterior = document.getElementById('valor-aterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
